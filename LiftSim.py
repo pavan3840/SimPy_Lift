a3a2d33e8b18c9cd05208d602e8f2316b13ae88c
pavan3840 <balijepalli.kumar@wipro.com>
2019-10-18 15:51:36 +0530

    Finalise the Lift Behaviour

--- LiftSim.py
+++ LiftSim.py
@@ -1,53 +1,69 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep 26 09:33:04 2019
+Created on Wed Oct 16 09:33:04 2019
 
-@author: BA20090925
+@author: iAmGamrr
 """
 import simpy
 import random
+
 curr = 0
-stat = True
-tran = False
+curr2 = 0
+stat1 = True
+stat2 = True
+tran1 = False
+tran2 = False
 floors = 10
-class Lift(object):
+
+class Lift_Simulation(object):
+    
     def liftFloorTrigger(env,Ftrigger):
-        global curr,stat,tran,floors
+        global curr,curr2,stat1,tran1,floors,move_time
+        
+        '''
+        This if condition will make sure the given floor is present in the building.
+        '''
         if Ftrigger>=0 and Ftrigger<=floors:
+            '''
+            This if condition will confirm if the lift is present at the same floor
+            as the requested floor.
+            '''
             if(curr!=Ftrigger):
                 '''
                 Here we have got that requested floor is not same as current 
                 floor of lift. So it will move from that place to requested place.
                 '''
                 print('Moving from ' + str(curr) + ' to ' + str(Ftrigger))
-                tran = True
-                stat = False
+                tran1 = True
+                stat1 = False
+                diff = abs(curr-Ftrigger)
                 curr = Ftrigger
-                move_time = Ftrigger*3
+                move_time = diff*3
                 yield env.timeout(move_time)
                 '''
                 Here, The lift took 3 unit time for 1 floor and reached the floor.
                 The door open time is 2 for people to get in and close time is 1 too.
                 '''
-                print('Reached ' + str(curr) + ' at ' + str(env.now) + ' second.')
+                print('Reached ' + str(curr) + ' at ' + str(env.now) + ' second. ' + str(diff) + ' floors')
                 
                 #Opening Door
                 door_open_time = 2;
                 yield env.timeout(door_open_time)
-                stat = True
-                tran = False
+                stat1 = True
+                tran1 = False
                 print('Doors opened at ' + str(env.now) + ' second. 2 units')
                 
                 #Closing Door
                 door_close_time = 1;
                 yield env.timeout(door_close_time)
                 print('Doors closed at ' + str(env.now) + ' second. 1 unit')
+                print()
             else:
                 '''
                 Here, The lift is on the requested floor, so the doors will open and wait
                 for a second and close back.
                 '''
-                print('Reached ' + str(curr) + ' at ' + str(env.now) + ' second.')
+                print('Reached ' + str(curr) + ' at ' + str(env.now) + ' second. ' + str(move_time) + ' units - same floor')
                 
                 #Opening Door
                 door_open_time = 2;
@@ -58,24 +74,24 @@ class Lift(object):
                 door_close_time = 1;
                 yield env.timeout(door_close_time)
                 print('Doors closed at ' + str(env.now) + ' second. 1 unit')
+                print()
         else:
             print('Only 10 floors are present')
-            
+                
     def liftInsideTrigger(env,Ltrigger):
-        global curr,stat,tran,floors
+        global curr,stat1,tran1,floors,move_time
         if Ltrigger>=0 and Ltrigger<=floors:
             if curr!=Ltrigger:
                 print ('Moving from' + str(curr))
-                tran = True
-                stat = False
+                tran1 = True
+                stat1 = False
                 curr = Ltrigger
-                move_time = Ltrigger*3
+                move_time = abs(Ltrigger-curr)*3
                 yield env.timeout(move_time)
                 print('Reached ' + str(curr) + ' at ' + str(env.now) + ' second.')
-                stat = True
-                tran = False
+                stat1 = True
+                tran1 = False
             else:
-                
                 #Opening Door
                 door_open_time = 2;
                 yield env.timeout(door_open_time)
@@ -84,9 +100,7 @@ class Lift(object):
         else:
             print('Only 10 floors are present')
 
-
-
-for i in range(4):
-    env = simpy.Environment()
-    env.process(Lift.liftFloorTrigger(env,random.randint(0,11)))
-    env.run(until=60)
+for i in range(20):
+    env = simpy.Environment()    
+    env.process(Lift_Simulation.liftFloorTrigger(env,random.randint(0,10)))
+    env.run()
